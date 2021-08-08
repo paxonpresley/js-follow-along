26626c5e8468e41ec3a61c186af6744f6d4fced9
paxonpresley <76454250+paxonpresley@users.noreply.github.com>
2021-08-07 19:51:33 -0500

    save

--- video-player/app.js
+++ video-player/app.js
@@ -1,7 +1,12 @@
 const preloader = document.querySelector('preloader');
+const btn = document.getElementById('#btn');
+const video = document.querySelector('.vid-container');
 
 // preloader
 window.onload = function() {
     document.querySelector('.preloader').style.display = "none";
 };
 
+btn.addEventListener('click', function(){
+    
+})
