18ceb0de385fcfa2908e7dfd52538de17cd8b5fa
paxonpresley <76454250+paxonpresley@users.noreply.github.com>
2021-08-08 22:29:56 -0500

    bugged

--- scroll/app.js
+++ scroll/app.js
@@ -6,6 +6,7 @@
 const date = document.getElementById('date');
 date.innerHTML = new Date().getFullYear();
 
+
 // ********** close links ************
 // ur looking for 3 different elements, the toggle btn, the links, and links container
 const navToggle = document.querySelector('.nav-toggle');
@@ -23,21 +24,22 @@ navToggle.addEventListener("click", function() {
         linksContainer.style.height = `${linksHeight}px`;
     } else {
         linksContainer.style.height = 0;
-    };
+    }
 });
+
+
 // ********** fixed navbar ************
 // todo: when you scroll past the height of the nav it pops up smoothly at the top. 1. add an eventListener set to scroll. 2. add function that when the page is scrolled so far you add a classList of '.fixed-nav' to the correct element. 3. now you want another function that at a certain scroll point you want to show the back to top btn.
 const navbar = document.getElementById('nav');
 const topLink = document.querySelector('.top-link');
-
 window.addEventListener('scroll', function() {
     // console.log(window.pageYOffset);
     const scrollHeight = window.pageYOffset;
     const navHeight = navbar.getBoundingClientRect().height;
     if (scrollHeight > navHeight) {
-        navbar.classList.add('.fixed-nav');
+        navbar.classList.add('fixed-nav');
     } else {
-        navbar.classList.remove('.fixed-nav');
+        navbar.classList.remove('fixed-nav');
     }
     // this is the if else function that we needed in step 3
     if (scrollHeight > 1000) {
@@ -46,9 +48,11 @@ window.addEventListener('scroll', function() {
         topLink.classList.remove('show-link');
     }
 });
+
+
 // ********** smooth scroll ************
 // select links
-const scrollLinks = document.querySelectorAll('.scroll-link');
+const scrollLinks = document.querySelectorAll('scroll-link');
 scrollLinks.forEach(function(link) { // 'link' is a param that you can access through the function
     link.addEventListener('click', function(e){
         // preventDefault means it stops the original functionality so you can program your own functionality
@@ -62,11 +66,16 @@ scrollLinks.forEach(function(link) { // 'link' is a param that you can access th
         const navHeight = links.getBoundingClientRect().height;
         const fixedNav = links.classList.contains('fixed-nav')
         let position = element.offsetTop - navHeight;
-        
-        console.log(position);
+        // console.log(position);
+        if(!fixedNav){
+            position = position - navHeight;
+        } if (navHeight > 82){
+            position = position + containerHeight;
+        }
         window.scrollTo({
-            top: position
-        });
+            left: 0,
+            top: position,
+        })
         linksContainer.style.height = 0;
     });
 });
