5ac058a632fdb60e4bd6f6a6d41b27df83402646
paxonpresley <76454250+paxonpresley@users.noreply.github.com>
2021-08-10 14:00:58 -0500

    js bug

--- tabs/app.js
+++ tabs/app.js
@@ -1,2 +1,9 @@
 // todo: when the window opens you want the default classList of .active on the history page
-const 
+
+const about = document.querySelector('.about');
+const tabBtns = document.querySelectorAll('.tab-btn');
+const articles = document.querySelectorAll('.content');
+
+about.addEventListener('click', function(e){
+    console.log(e.target);
+});
