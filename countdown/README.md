8781e74c7a5d4d997ec899c27e748d16db7a1d02
paxonpresley <76454250+paxonpresley@users.noreply.github.com>
2021-08-11 20:33:01 -0500

    saved changes

---
+++ countdown/README.md
@@ -0,0 +1,2 @@
+wording what i want to do before the step by step code
+#! declare 2 arrays with the name of the months and weekdays 
--- countdown/app.js
+++ countdown/app.js
@@ -1,1 +1,27 @@
-const 
+//! arrays that i don't really understand why they're here yet
+
+const months = [
+    "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const weekdays = [
+    "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const giveawayDate = document.querySelector('.giveaway-date')
