062afeff4f2b8ee878063439e7c57e303d465dea
paxonpresley <76454250+paxonpresley@users.noreply.github.com>
2021-08-12 20:40:31 -0500

    saved js #4

--- countdown/app.js
+++ countdown/app.js
@@ -53,9 +53,43 @@ giveawayDate.textContent = `${weekday}, ${month} ${date}, ${year} at ${hours}:${
 // *future time in milliseconds
 const futureTime = dateDate.getTime();
 // console.log(futureTime);
+
 function timeCalc (){
     const today = new Date().getTime();
-    console.log(today);
-}
+    // console.log(today);
+    // ! t will be calculated in milliseconds
+    const t = dateDate - today;
+    // console.log(t);
+    // * 1s = 1000ms
+    // * 1min = 60sec
+    // * 1hr = 60min
+    // * 1 day = 24hrs
+
+    // this equation is going to give you a number that counts one day in milliseconds
+    const oneDay = 24*60*60*1000;
+    // console.log(oneDay);
+    const oneHour = 60*60*1000;
+    const oneMinute = 60*1000;
+    const oneSecond = 1000;
+
+    // * long way of doing things
+        // let days = t - oneDay;
+        // days = Math.floor(days);
+        // let hours = t % oneDay / oneHour;
+        // hours = Math.floor(hours);
+        // // console.log(hours);
+        // let minutes = t % oneDay % oneHour / oneMinute;
+        // minutes = Math.floor(minutes);
+        // // console.log(minutes);
+        // let seconds = t % oneDay % oneHour % oneMinute / 1000;
+        // seconds = Math.floor(seconds);
+        // // console.log(seconds);
+
+    let days = Math.floor(t - oneDay);
+    console.log(days);
+    let hours = Math.floor(t % oneDay / oneHour);
+    let minutes = Math.floor(t % oneHour / oneMinute);
+    let seconds = Math.floor(t % oneMinute / oneSecond);
+};
 
 timeCalc();
