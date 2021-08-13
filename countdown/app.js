2cded65b00169c14e65acb8faf0aa5374faa76fa
paxonpresley <76454250+paxonpresley@users.noreply.github.com>
2021-08-12 19:25:42 -0500

    saved js #3

--- countdown/app.js
+++ countdown/app.js
@@ -2,31 +2,60 @@
 
 const months = [
     "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
 ];
 const weekdays = [
     "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
 ];
 
 const giveawayDate = document.querySelector('.giveaway-date');
 const timerContainer = document.querySelector('.timer-container');
 const boxH5 = document.querySelectorAll('.box h5');
-console.log(boxH5);
-var giveawayDate = new Date(2022, 10, 17, 30);
-console.log(giveawayDate);
+// console.log(boxH5);
+
+//* declared giveaway date
+var dateDate = new Date('September 15, 2022 12:22:32');
+// console.log(giveawayDate);
+//* end declared giveaway date
+
+//* my year, month, hour, etc that gets that attribute and acts as a middle man
+const year = dateDate.getFullYear();
+const hours = dateDate.getHours();
+const min = dateDate.getMinutes();
+
+let month = dateDate.getMonth();
+// console.log(months(month));
+month = months[month];
+
+const date = dateDate.getDate();
+
+const weekday = weekdays[dateDate.getDay()];
+// console.log(weekday);
+
+giveawayDate.textContent = `${weekday}, ${month} ${date}, ${year} at ${hours}:${min}am`;
+
+// *future time in milliseconds
+const futureTime = dateDate.getTime();
+// console.log(futureTime);
+function timeCalc (){
+    const today = new Date().getTime();
+    console.log(today);
+}
+
+timeCalc();
